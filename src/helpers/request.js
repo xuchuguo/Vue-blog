cab8471d6c5e219bdb589e53fd16dcb8293f093a
xuchuguo <xuchuguo@qq.com>
2020-11-18 22:41:40 +0800

    修改代码

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -3,7 +3,7 @@ import { Message } from 'element-ui'
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 axios.defaults.baseURL = 'https://blog-server.hunger-valley.com'
-axios.defaults.withCredentials = true
+// axios.defaults.withCredentials = true
 
 export default function request(url, type = 'GET', data = {}) {
   return new Promise((resolve, reject) => {
